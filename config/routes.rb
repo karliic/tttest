73d9650d4b561d9cb797b138b940cf8ea22c686e
Karlis <karlis@karlis-VirtualBox.(none)>
2013-05-06 23:46:52 +0300

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Skolai::Application.routes.draw do
   resources :users
 
+  root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +52,7 @@ Skolai::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
