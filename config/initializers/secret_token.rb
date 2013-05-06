1581dfd689092738d664afb745f69e07c3499fd3
Karlis <karlis@karlis-VirtualBox.(none)>
2013-05-06 23:40:51 +0300

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skolai::Application.config.secret_token = 'affd212cd38fd6b6c29de68666e7c2adcf84b6f577c044d200ec29c2c4589114eb77baf03fe0eca5001690b7100c910367d5512885b7b401bcfa7dfdb970d8d2'
